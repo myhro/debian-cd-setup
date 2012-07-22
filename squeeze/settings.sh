cc9b4736b77b2586df7752fb16a125504b84ada8
cd-builder user <debian-cd@lists.debian.org>
2012-07-22 02:01:09 +0000

    More re-org to cope with dir move

--- squeeze/settings.sh
+++ squeeze/settings.sh
@@ -5,7 +5,7 @@ export HOSTNAME=`hostname -f`
 export PUBDIR=/org/cdbuilder.debian.org/dst/deb-cd
 
 export MIRROR=/org/cdbuilder.debian.org/src/ftp/debian
-export BASEDIR=~/build/debian-cd
+export BASEDIR=~/build.squeeze/debian-cd
 export MKISOFS=~/build/mkisofs/usr/bin/mkisofs
 if [ "$DATE"x = ""x ] ; then
     export DATE=`date -u +%Y%m%d`
