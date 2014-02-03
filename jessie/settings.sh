fa710d3d5cbfe013ee95e5baaa1fb4d304951cd2
Steve McIntyre <steve@einval.com>
2014-02-03 11:28:48 +0000

    ia64 is no longer in jessie, so remove it from our arch list

--- jessie/settings.sh
+++ jessie/settings.sh
@@ -14,11 +14,11 @@ fi
 export EXTRACTED_SOURCES=/mnt/nfs-cdimage/cd-sources
 
 if [ "$ARCHES"x = ""x ] ; then
-    ARCHES="i386 source amd64 multi-arch powerpc armel armhf ia64 mips mipsel s390x sparc kfreebsd-amd64 kfreebsd-i386"
+    ARCHES="i386 source amd64 multi-arch powerpc armel armhf mips mipsel s390x sparc kfreebsd-amd64 kfreebsd-i386"
 fi
 
 if [ "$ARCHES_FIRMWARE"x = ""x ] ; then
-    ARCHES_FIRMWARE="amd64 i386 ia64 powerpc multi-arch"
+    ARCHES_FIRMWARE="amd64 i386 powerpc multi-arch"
 #    ARCHES_FIRMWARE="amd64"
 fi
 
